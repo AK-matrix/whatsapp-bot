a7251dde9d0d89cc67fa1119cd4240a0eddef30c
AK-matrix <arnavkamath2802@gmail.com>
2025-07-31 22:58:24 +0530

    fix

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
+
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
